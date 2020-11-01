94c450586119328d6c2a7d6bca9c07b77c4bc902
joppe.leers <joppe.leers@student.kuleuven.be>
2020-11-01 17:24:20 +0100

    changed .env file -> add your own URL to it + finished the layout for the template

--- app/Controllers/Maincontroller.php
+++ app/Controllers/Maincontroller.php
@@ -6,15 +6,30 @@ namespace App\Controllers;
 
 class Maincontroller extends \CodeIgniter\Controller
 {
-    private $data;
-
-    public function __construct()
-    {
-    }
 
     public function leaderboardSelect() {
-        $this->data['header_icon_1'] = 'arrow_back';
-        $this->data['header_icon_2'] = 'search';
-        return view("mainTemplate", $this->data);
+        $data['header_icon_1'] = 'arrow_back';
+        $data['header_middle'] = 'search bar?';
+        $data['header_icon_2'] = 'search';
+        return view("mainTemplate", $data);
+    }
+
+    public function hub() {
+        $data['header_icon_1'] = 'arrow_back';
+        $data['header_middle'] = 'search bar?';
+        $data['header_icon_2'] = 'search';
+        return view("mainTemplate", $data);
+    }
+
+    public function groups() {
+        return view("mainTemplate");
+    }
+
+    public function profile() {
+        return view("mainTemplate");
+    }
+
+    public function addObservation() {
+        return view("mainTemplate");
     }
 }
--- app/Views/mainTemplate.php
+++ app/Views/mainTemplate.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="keywords" content="Animals Plants Nature Social media" />
     <meta name="description" content="This website is a social madia like page where you can share your observations from nature with your friends and the world" />
-    <link href="<?= base_url()?>/public/css/main.css?version=2" rel="stylesheet" type="text/css"/>
+    <link href="<?= base_url()?>/public/css/main.css?version=4" rel="stylesheet" type="text/css"/>
     <!-- <link href="<?= base_url()?>/public/css/media_query.css" rel="stylesheet"> -->
 
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -13,21 +13,23 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto&display=swap" rel="stylesheet">
 </head>
 <body>
-<p>Hallo Wat denk je van dit font?</p>
-<section>
+<div>
     <header>
-        <button class="material-icons"><?=$header_icon_1?></button>
-        <button class="material-icons"><?=$header_icon_2?></button>
+        <span class="material-icons" id="header_icon_1"><?=$header_icon_1?></span>
+        <span class="material-icons" id="header_icon_2"><?=$header_icon_2?></span>
     </header>
     <main>
-        <p>Hallo Wat denk je van dit font?</p>
-        <h1>Dit is header 2</h1>
-        <h2>Dit is header 2</h2>
+
     </main>
     <footer>
         <nav>
+            <a href="hub" title="Go to Hub" class="inactive"><span class="material-icons">home</span></a>
+            <a href="groups" title="Go to groups" class="inactive"><span class="material-icons">people_alt</span></a>
+            <a href="addObservation" title="Go to addObservation" class="active"><span class="material-icons">add_circle</span></a></li>
+            <a href="leaderboardSelect" title="Go to leaderboardSelect" class="inactive"><span class="material-icons">leaderboard</span></a></li>
+            <a href="profile" title="Go to profile" class="inactive"><span class="material-icons">person</span></a></li>
         </nav>
     </footer>
-</section>
+</div>
 </body>
 </html>
