faceb82f4ad7819bbd59e583a08d141d9b361cd8
toon.luykx <toon.luykx@student.kuleuven.be>
2020-11-04 20:27:38 +0100

    added friendList and search page to develop

---
+++ app/Views/friendList.php
@@ -0,0 +1,58 @@
+<div class="container-fluid">
+    <div class="row_main">
+        <div class="col-md-12">
+            <div class="personCardContainer">
+                <div class="personCardPhotoContainer">
+                    <img class="personCardPhoto" alt="Bootstrap Image Preview" src="https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png">
+                </div>
+                <div class="personCardInfo">
+                    <h3>
+                        Firstname Lastname
+                    </h3>
+                    <address>
+                        <strong>Twitter, Inc.</strong><br> 795 Folsom Ave, Suite 600<br> San Francisco, CA 94107<br> <abbr title="Phone">P:</abbr> (123) 456-7890
+                    </address>
+                </div>
+            </div>
+            <div class="personCardContainer">
+                <div class="personCardPhotoContainer">
+                    <img class="personCardPhoto" alt="Bootstrap Image Preview" src="https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png">
+                </div>
+                <div class="personCardInfo">
+                    <h3>
+                        Firstname Lastname
+                    </h3>
+                    <address>
+                        <strong>Twitter, Inc.</strong><br> 795 Folsom Ave, Suite 600<br> San Francisco, CA 94107<br> <abbr title="Phone">P:</abbr> (123) 456-7890
+                    </address>
+                </div>
+            </div>
+            <div class="personCardContainer">
+                <div class="personCardPhotoContainer">
+                    <img class="personCardPhoto" alt="Bootstrap Image Preview" src="https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png">
+                </div>
+                <div class="personCardInfo">
+                    <h3>
+                        Firstname Lastname
+                    </h3>
+                    <address>
+                        <strong>Twitter, Inc.</strong><br> 795 Folsom Ave, Suite 600<br> San Francisco, CA 94107<br> <abbr title="Phone">P:</abbr> (123) 456-7890
+                    </address>
+                </div>
+            </div>
+            <div class="personCardContainer">
+                <div class="personCardPhotoContainer">
+                    <img class="personCardPhoto" alt="Bootstrap Image Preview" src="https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png">
+                </div>
+                <div class="personCardInfo">
+                    <h3>
+                        Firstname Lastname
+                    </h3>
+                    <address>
+                        <strong>Twitter, Inc.</strong><br> 795 Folsom Ave, Suite 600<br> San Francisco, CA 94107<br> <abbr title="Phone">P:</abbr> (123) 456-7890
+                    </address>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
