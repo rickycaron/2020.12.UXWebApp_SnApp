388329a99eb08238c12d94dead21e545e6b12fe9
beiyang.li <beiyang.li@student.kuleuven.be>
2020-11-05 23:08:59 +0100

    Beiyang Li page First version

---
+++ app/Views/profile.php
@@ -0,0 +1,179 @@
+
+<div class="profileUserContainer">        
+        <div class="col-sm-4 col-lg-4 col-md-4">
+            <img src="https://pic4.zhimg.com/ee44507a59989947c85d60e0b400f0c5_xl.jpg" class="img-circle" alt="templatemo easy profile" style="width: 100px;">
+                
+        </div>
+
+        <div class="col-sm-8 col-lg-8 col-md-8">
+            <h1 class="user_name">Beiyang Li</h1>
+            <h1 class="personal_description">A guy who really likes photography. XXXXXXXXXXXXXXXXXXXXXXXXX XXXXXX</h1>
+            <div class = "trophyContainer">
+                <span class="material-icons trophy">emoji_events</span>
+                <span class="material-icons trophy">emoji_events</span>
+                <span class="material-icons trophy">emoji_events</span>
+                <span class="material-icons trophy">emoji_events</span>
+            </div>
+
+
+        </div>
+
+</div>
+
+
+<div id = "profile_data">
+
+    <div id = "profile_information">
+        
+        <span class = "profileInformation col-sm-3 col-md-3 col-lg-3" > observations</span>
+        <span class = "profileInformation col-sm-2 col-md-2 col-lg-2" > likes</span>
+        <span class = "profileInformation col-sm-3 col-md-3 col-lg-3" > comments</span>
+        <span class = "profileInformation col-sm-2 col-md-2 col-lg-2" > friends</span>
+        <span class = "profileInformation col-sm-2 col-md-2 col-lg-2" > points</span>
+
+    </div>
+
+    <div id = "profile_number">
+        
+        <span class = "profileData col-sm-3 col-md-3 col-lg-3" > 103</span>
+        <span class = "profileData col-sm-2 col-md-2 col-lg-2" > 222</span>
+        <span class = "profileData col-sm-3 col-md-3 col-lg-3" > 78</span>
+        <span class = "profileData col-sm-2 col-md-2 col-lg-2" > 99</span>
+        <span class = "profileData col-sm-2 col-md-2 col-lg-2" > 101</span>
+
+    </div>
+
+    
+
+
+</div>
+
+
+<div id="observationCardContainer" class="col-sm-12 col-md-10 col-lg-7">
+    <div id="observationCard_p">
+        <div id="observationCardProfile_profile">
+            <img id="ProfilePicture" src="https://pic4.zhimg.com/ee44507a59989947c85d60e0b400f0c5_xl.jpg" style="width: 35px;border-radius: 35px">
+
+            <h3 id="profileName"> Beiyang Li </h3>
+        </div>
+
+        <div id="observationCardPicture">
+            <img id="observationPicture" src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-image-512.png">
+        </div>
+
+        <div id="observationCardPictureFooter">
+            <div id="observationCardPictureFooterLocation">
+                <span class="material-icons">location_on</span>
+                <h4>
+                    Leuven
+                </h4>
+            </div>
+            <div id="observationCardPictureFooterButtons">
+                <span class="material-icons">favorite_border</span>
+                <span class="material-icons">chat</span>
+            </div>
+        </div>
+
+        <div id="observationCardComments_p">
+            <ul>
+                <li id="observationCardCommentsOne">
+                    Joppe: A beautiful flower!
+                </li>
+                <li id="observationCardCommentsOne">
+                    Rui: Wow great capture!
+                </li>
+            </ul>
+        </div>
+
+        <div id="observationCardTime_p">
+            <h4>5 hours ago</h4>
+        </div>
+    </div>
+</div>
+
+
+<div id="observationCardContainer" class="col-sm-12 col-md-10 col-lg-7">
+    <div id="observationCard_p">
+        <div id="observationCardProfile_profile">
+            <img id="ProfilePicture" src="https://pic4.zhimg.com/ee44507a59989947c85d60e0b400f0c5_xl.jpg" style="width: 35px;border-radius: 35px">
+
+            <h3 id="profileName"> Beiyang Li </h3>
+        </div>
+
+        <div id="observationCardPicture">
+            <img id="observationPicture" src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-image-512.png">
+        </div>
+
+        <div id="observationCardPictureFooter">
+            <div id="observationCardPictureFooterLocation">
+                <span class="material-icons">location_on</span>
+                <h4>
+                    Leuven
+                </h4>
+            </div>
+            <div id="observationCardPictureFooterButtons">
+                <span class="material-icons">favorite_border</span>
+                <span class="material-icons">chat</span>
+            </div>
+        </div>
+
+        <div id="observationCardComments_p">
+            <ul>
+                <li id="observationCardCommentsOne">
+                    Joppe: A beautiful flower!
+                </li>
+                <li id="observationCardCommentsOne">
+                    Rui: Wow great capture!
+                </li>
+            </ul>
+        </div>
+
+        <div id="observationCardTime_p">
+            <h4>5 hours ago</h4>
+        </div>
+    </div>
+</div>
+
+
+<div id="observationCardContainer" class="col-sm-12 col-md-10 col-lg-7">
+    <div id="observationCard_p">
+        <div id="observationCardProfile_profile">
+            <img id="ProfilePicture" src="https://pic4.zhimg.com/ee44507a59989947c85d60e0b400f0c5_xl.jpg" style="width: 35px;border-radius: 35px">
+
+            <h3 id="profileName"> Beiyang Li </h3>
+        </div>
+
+        <div id="observationCardPicture">
+            <img id="observationPicture" src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-image-512.png">
+        </div>
+
+        <div id="observationCardPictureFooter">
+            <div id="observationCardPictureFooterLocation">
+                <span class="material-icons">location_on</span>
+                <h4>
+                    Leuven
+                </h4>
+            </div>
+            <div id="observationCardPictureFooterButtons">
+                <span class="material-icons">favorite_border</span>
+                <span class="material-icons">chat</span>
+            </div>
+        </div>
+
+        <div id="observationCardComments_p">
+            <ul>
+                <li id="observationCardCommentsOne">
+                    Joppe: A beautiful flower!
+                </li>
+                <li id="observationCardCommentsOne">
+                    Rui: Wow great capture!
+                </li>
+            </ul>
+        </div>
+
+        <div id="observationCardTime_p">
+            <h4>5 hours ago</h4>
+        </div>
+    </div>
+</div>
+
