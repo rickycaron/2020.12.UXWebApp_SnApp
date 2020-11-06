888a53c3f9930ee5fec21e44c9c5edf3b6ace13a
beiyang.li <beiyang.li@student.kuleuven.be>
2020-11-06 15:06:28 +0100

    final sprint 5

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -1,22 +1,21 @@
+<div class="profileUserContainer">
+    <div class="col-sm-4 col-lg-4 col-md-4">
+        <img src="https://pic4.zhimg.com/ee44507a59989947c85d60e0b400f0c5_xl.jpg" class="img-circle" alt="templatemo easy profile" style="width: 100px;">
 
-<div class="profileUserContainer">        
-        <div class="col-sm-4 col-lg-4 col-md-4">
-            <img src="https://pic4.zhimg.com/ee44507a59989947c85d60e0b400f0c5_xl.jpg" class="img-circle" alt="templatemo easy profile" style="width: 100px;">
-                
+    </div>
+
+    <div class="col-sm-8 col-lg-8 col-md-8">
+        <h1 class="user_name">Beiyang Li</h1>
+        <h1 class="personal_description">A guy who really likes photography. XXXXXXXXXXXXXXXXXXXXXXXXX XXXXXX</h1>
+        <div class = "trophyContainer">
+            <span class="material-icons trophy">emoji_events</span>
+            <span class="material-icons trophy">emoji_events</span>
+            <span class="material-icons trophy">emoji_events</span>
+            <span class="material-icons trophy">emoji_events</span>
         </div>
 
-        <div class="col-sm-8 col-lg-8 col-md-8">
-            <h1 class="user_name">Beiyang Li</h1>
-            <h1 class="personal_description">A guy who really likes photography. XXXXXXXXXXXXXXXXXXXXXXXXX XXXXXX</h1>
-            <div class = "trophyContainer">
-                <span class="material-icons trophy">emoji_events</span>
-                <span class="material-icons trophy">emoji_events</span>
-                <span class="material-icons trophy">emoji_events</span>
-                <span class="material-icons trophy">emoji_events</span>
-            </div>
 
-
-        </div>
+    </div>
 
 </div>
 
@@ -24,7 +23,7 @@
 <div id = "profile_data">
 
     <div id = "profile_information">
-        
+
         <span class = "profileInformation col-sm-3 col-md-3 col-lg-3" > observations</span>
         <span class = "profileInformation col-sm-2 col-md-2 col-lg-2" > likes</span>
         <span class = "profileInformation col-sm-3 col-md-3 col-lg-3" > comments</span>
@@ -34,7 +33,7 @@
     </div>
 
     <div id = "profile_number">
-        
+
         <span class = "profileData col-sm-3 col-md-3 col-lg-3" > 103</span>
         <span class = "profileData col-sm-2 col-md-2 col-lg-2" > 222</span>
         <span class = "profileData col-sm-3 col-md-3 col-lg-3" > 78</span>
@@ -43,25 +42,27 @@
 
     </div>
 
-    
+
 
 
 </div>
 
 
-<div id="observationCardContainer_p" class="col-sm-12 col-md-10 col-lg-7">
+
+
+<div id="observationCardContainer_p" class="col-sm-12 col-md-11 col-lg-11">
     <div id="observationCard_p">
         <div id="observationCardProfile_profile">
-            <img id="ProfilePicture" src="https://pic4.zhimg.com/ee44507a59989947c85d60e0b400f0c5_xl.jpg" style="width: 35px;border-radius: 35px">
+            <img id="ProfilePicture" src="https://pic3.zhimg.com/v2-cbac9e5b1db66e9a1c0e7c45703a1aa3_xl.jpg" style="width: 35px;border-radius: 35px">
 
             <h3 id="profileName"> Beiyang Li </h3>
         </div>
 
-        <div id="observationCardPicture">
+        <div id="observationCardPicture_p">
             <img id="observationPicture" src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-image-512.png">
         </div>
 
-        <div id="observationCardPictureFooter">
+        <div id="observationCardPictureFooter_p">
             <div id="observationCardPictureFooterLocation">
                 <span class="material-icons">location_on</span>
                 <h4>
@@ -76,11 +77,11 @@
 
         <div id="observationCardComments_p">
             <ul>
-                <li id="observationCardCommentsOne">
-                    Joppe: A beautiful flower!
+                <li id="observationCardCommentsComment">
+                    joppeleers: Wow what a nice flower!
                 </li>
-                <li id="observationCardCommentsOne">
-                    Rui: Wow great capture!
+                <li id="observationCardCommentsComment">
+                    robbeabts: awesome!
                 </li>
             </ul>
         </div>
@@ -92,19 +93,19 @@
 </div>
 
 
-<div id="observationCardContainer_p" class="col-sm-12 col-md-10 col-lg-7">
+<div id="observationCardContainer_p" class="col-sm-12 col-md-11 col-lg-11">
     <div id="observationCard_p">
         <div id="observationCardProfile_profile">
-            <img id="ProfilePicture" src="https://pic4.zhimg.com/ee44507a59989947c85d60e0b400f0c5_xl.jpg" style="width: 35px;border-radius: 35px">
+            <img id="ProfilePicture" src="https://pic3.zhimg.com/v2-cbac9e5b1db66e9a1c0e7c45703a1aa3_xl.jpg" style="width: 35px;border-radius: 35px">
 
             <h3 id="profileName"> Beiyang Li </h3>
         </div>
 
-        <div id="observationCardPicture">
+        <div id="observationCardPicture_p">
             <img id="observationPicture" src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-image-512.png">
         </div>
 
-        <div id="observationCardPictureFooter">
+        <div id="observationCardPictureFooter_p">
             <div id="observationCardPictureFooterLocation">
                 <span class="material-icons">location_on</span>
                 <h4>
@@ -119,11 +120,11 @@
 
         <div id="observationCardComments_p">
             <ul>
-                <li id="observationCardCommentsOne">
-                    Joppe: A beautiful flower!
+                <li id="observationCardCommentsComment">
+                    joppeleers: Wow what a nice flower!
                 </li>
-                <li id="observationCardCommentsOne">
-                    Rui: Wow great capture!
+                <li id="observationCardCommentsComment">
+                    robbeabts: awesome!
                 </li>
             </ul>
         </div>
@@ -132,22 +133,19 @@
             <h4>5 hours ago</h4>
         </div>
     </div>
-</div>
-
-
-<div id="observationCardContainer_p" class="col-sm-12 col-md-10 col-lg-7">
+</div><div id="observationCardContainer_p" class="col-sm-12 col-md-11 col-lg-11">
     <div id="observationCard_p">
         <div id="observationCardProfile_profile">
-            <img id="ProfilePicture" src="https://pic4.zhimg.com/ee44507a59989947c85d60e0b400f0c5_xl.jpg" style="width: 35px;border-radius: 35px">
+            <img id="ProfilePicture" src="https://pic3.zhimg.com/v2-cbac9e5b1db66e9a1c0e7c45703a1aa3_xl.jpg" style="width: 35px;border-radius: 35px">
 
             <h3 id="profileName"> Beiyang Li </h3>
         </div>
 
-        <div id="observationCardPicture">
+        <div id="observationCardPicture_p">
             <img id="observationPicture" src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-image-512.png">
         </div>
 
-        <div id="observationCardPictureFooter">
+        <div id="observationCardPictureFooter_p">
             <div id="observationCardPictureFooterLocation">
                 <span class="material-icons">location_on</span>
                 <h4>
@@ -162,11 +160,11 @@
 
         <div id="observationCardComments_p">
             <ul>
-                <li id="observationCardCommentsOne">
-                    Joppe: A beautiful flower!
+                <li id="observationCardCommentsComment">
+                    joppeleers: Wow what a nice flower!
                 </li>
-                <li id="observationCardCommentsOne">
-                    Rui: Wow great capture!
+                <li id="observationCardCommentsComment">
+                    robbeabts: awesome!
                 </li>
             </ul>
         </div>
@@ -176,4 +174,3 @@
         </div>
     </div>
 </div>
-
