2120086ae8f448d6ce65ef0a91565edeb148752b
maarten.kesters <maarten.kesters@student.kuleuven.be>
2020-11-13 15:03:22 +0100

    fixed addobservation

--- html/js/previewPicture.js
+++ html/js/previewPicture.js
@@ -3,7 +3,14 @@ function readURL(input) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
+
+            document.getElementById("backupIcon").style.display = 'none';
+            document.getElementById("noFileText").style.display = 'none';
+            document.getElementById("previewImageDiv").style.display = 'flex';
+
             $('#uploadImageTag').attr('src', e.target.result);
+
+            sendIdentification();
         };
         reader.readAsDataURL(input.files[0]);
     }
