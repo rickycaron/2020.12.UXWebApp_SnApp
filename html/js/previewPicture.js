265c2754becd0f42c57bbfac1ae9b9c224be4cc6
maarten.kesters <maarten.kesters@student.kuleuven.be>
2020-11-20 16:04:50 +0100

    changed button addobservation

--- html/js/previewPicture.js
+++ html/js/previewPicture.js
@@ -7,6 +7,7 @@ function readURL(input) {
             document.getElementById("backupIcon").style.display = 'none';
             document.getElementById("noFileText").style.display = 'none';
             document.getElementById("previewImageDiv").style.display = 'flex';
+            document.getElementById("takePictureButton").innerHTML = 'Take another picture';
 
             $('#uploadImageTag').attr('src', e.target.result);
 
