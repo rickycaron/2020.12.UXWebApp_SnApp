a0b50ede1b3508f9fbf55eed535f810134d227a2
rui.zhu <rui.zhu@student.kuleuven.be>
2020-11-25 12:40:03 +0100

    smallchange

--- app/Views/groupsOverviewPage.php
+++ app/Views/groupsOverviewPage.php
@@ -7,27 +7,8 @@
             <label>Search</label>
         </div>
     </form>
-    <div onclick="location.href='group';" id="groupCard">
-        <div id="groupCardName">
-            <h2 id="groupName"> UXWD6 </h2>
-        </div>
-        <div id="groupCardInformation">
-            <div id="groupCardPicture">
-                <img id="observationPicture" src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-image-512.png">
-            </div>
-            <div id="groupInformationText">
-                <div id="groupDescription">
-                    <h4 id="groupDescriptionHeader">Description: </h4>
-                    <p id="groupDescriptionInfo">Observations from the members of UXWD6</p>
-                </div>
-                <div id="groupMembers">
-                    <h4 id="groupMembersHeader">Members: </h4>
-                    <p id="groupMembersInfo">6</p>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
+
 <div onclick="location.href='group';" id="groupCardContainer">
     <div id="groupCard">
         <div id="groupCardName">
