e418ab6460086cb47b02743a8dbf309735f77afb
rui.zhu <rui.zhu@student.kuleuven.be>
2020-11-26 11:59:57 +0100

    merge css

--- app/Views/groupPage.php
+++ app/Views/groupPage.php
@@ -1,76 +1,62 @@
-<div id="observationCardContainer">
-    <div id="groupTitle">
-        <h1 style="align-content: center">Groupname: UXWD6</h1>
-    </div>
-    <div id="observationCard">
-        <div id="observationCardProfile">
-            <img id="profilePicture" src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"> <! -- icons need to be imported in correct way!!! -->
-            <h3 id="profileName"> maartenkesters </h3>
-        </div>
-        <div id="observationCardPicture">
-            <img id="observationPicture" src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-image-512.png">
-        </div>
-        <div id="observationCardPictureFooter">
-            <div id="observationCardPictureFooterLocation">
-                <span class="material-icons">location_on</span>
-                <h4>
-                    Leuven
-                </h4>
-            </div>
-            <div id="observationCardPictureFooterButtons">
-                <span class="material-icons">favorite_border</span>
-                <span class="material-icons">chat</span>
-            </div>
-        </div>
-        <div id="observationCardComments">
-            <ul>
-                <li id="observationCardCommentsComment">
-                    joppeleers: Wow what a nice flower!
-                </li>
-                <li id="observationCardCommentsComment">
-                    robbeabts: awesome!
-                </li>
-            </ul>
-        </div>
-        <div id="observationCardTime">
-            <h4>5 hours ago</h4>
-        </div>
-    </div>
-</div>
+<div class="" style="width:100%;max-width:600px">
 
-<div id="observationCardContainer">
-    <div id="observationCard">
-        <div id="observationCardProfile">
-            <img id="profilePicture" src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"> <! -- icons need to be imported in correct way!!! -->
-            <h3 id="profileName"> maartenkesters </h3>
+    <h2 class="text-center my-4">Group page</h2>
+
+    <div class="card my-2 shadow-sm" style="width:100%;max-width:600px">
+
+        <div style="position: relative;">
+            <img class="card-img" style="" src="https://www.hunebednieuwscafe.nl/wp-content/uploads/2016/06/acorns-1579616_1280.jpg">
+            <div class="card-img" style="box-shadow: inset 0px -50px 40px -20px black;position: absolute; width: 100%; height: 100%;top: 0; left: 0;"></div>
+            <h4 class="text-white" style="position: absolute; bottom: 0px; right: 12px;"> <?= session()->get('username')?> </h4>
         </div>
-        <div id="observationCardPicture">
-            <img id="observationPicture" src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-image-512.png">
-        </div>
-        <div id="observationCardPictureFooter">
-            <div id="observationCardPictureFooterLocation">
-                <span class="material-icons">location_on</span>
-                <h4>
-                    Leuven
-                </h4>
+
+        <div class="card-body pt-2 pb-0">
+            <div class=" d-flex flex-row py-1">
+                <div class="mr-auto">
+                    <h3 class="mb-0">Common Name</h3>
+                    <h5 style="color:#6c757d;">Location</h5>
+                </div>
+                <span class="material-icons my-auto" style="font-size: 40px">expand_more</span>
             </div>
-            <div id="observationCardPictureFooterButtons">
-                <span class="material-icons">favorite_border</span>
-                <span class="material-icons">chat</span>
-            </div>
-        </div>
-        <div id="observationCardComments">
-            <ul>
-                <li id="observationCardCommentsComment">
-                    joppeleers: Wow what a nice flower!
-                </li>
-                <li id="observationCardCommentsComment">
-                    robbeabts: awesome!
-                </li>
-            </ul>
-        </div>
-        <div id="observationCardTime">
-            <h4>5 hours ago</h4>
         </div>
     </div>
+
+    <div class="card my-2 shadow-sm" style="width:100%;max-width:600px">
+
+        <div style="position: relative;">
+            <img class="card-img" style="" src="https://cdn.britannica.com/84/73184-004-E5A450B5/Sunflower-field-Fargo-North-Dakota.jpg">
+            <div class="card-img" style="box-shadow: inset 0px -50px 40px -20px black;position: absolute; width: 100%; height: 100%;top: 0; left: 0;"></div>
+            <h4 class="text-white" style="position: absolute; bottom: 0px; right: 12px;"> <?= session()->get('username')?> </h4>
+        </div>
+
+        <div class="card-body pt-2 pb-0">
+            <div class=" d-flex flex-row py-1">
+                <div class="mr-auto">
+                    <h3 class="mb-0">Common Name</h3>
+                    <h5 style="color:#6c757d;">Location</h5>
+                </div>
+                <span class="material-icons my-auto" style="font-size: 40px">expand_more</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="card my-2 shadow-sm" style="width:100%;max-width:600px">
+
+        <div style="position: relative;">
+            <img class="card-img" style="" src="https://dkt6rvnu67rqj.cloudfront.net/cdn/ff/T8cy0-640W8sartvA9TWmv08NbGPFxsVvf8gFtBDE08/1577112797/public/styles/600x400/public/media/int_files/elephant_in_tanzania.jpg?h=f507d761&itok=Ei8OXcGi">
+            <div class="card-img" style="box-shadow: inset 0px -50px 40px -20px black;position: absolute; width: 100%; height: 100%;top: 0; left: 0;"></div>
+            <h4 class="text-white" style="position: absolute; bottom: 0px; right: 12px;"> <?= session()->get('username')?> </h4>
+        </div>
+
+        <div class="card-body pt-2 pb-0">
+            <div class=" d-flex flex-row py-1">
+                <div class="mr-auto">
+                    <h3 class="mb-0">Common Name</h3>
+                    <h5 style="color:#6c757d;">Location</h5>
+                </div>
+                <span class="material-icons my-auto" style="font-size: 40px">expand_more</span>
+            </div>
+        </div>
+    </div>
+
 </div>
