433732535288c17bb908ebfa858795f0ed05aa31
beiyang.li <beiyang.li@student.kuleuven.be>
2020-12-04 14:52:16 +0100

    change another like icons

--- app/Views/hubPage.php
+++ app/Views/hubPage.php
@@ -31,14 +31,14 @@
                     <img class="card-img" id="observationCardPicture" src="<?php echo data_uri($ob->imageData,$ob->imageType); ?>">
                     <div class="card-img" style="box-shadow: inset 0px -50px 40px -20px black;position: absolute; width: 100%; height: 100%;top: 0; left: 0;"></div>
                     <h4 class="text-white" style="position: absolute; bottom: 0px; right: 12px;"><?=$ob->username?></h4>
-                    <div class="material-icons text-white" style="font-size:30px;position: absolute; bottom: 6px; left: 8px" >favorite_border</div>
+                    <div class="material-icons text-white" style="font-size:30px;position: absolute; bottom: 6px; left: 8px" >favorite</div>
                     </a>
 
                     <?php if ($likeStatus == 1): ?>
-                        <div class="material-icons text-danger likeButton" style="font-size:30px;position: absolute; bottom: 6px; left: 8px">favorite_border</div>
+                        <div class="material-icons text-danger likeButton" style="font-size:30px;position: absolute; bottom: 6px; left: 8px">favorite</div>
                     <?php endif;?>
                     <?php if ($likeStatus == 0): ?>
-                        <div class="material-icons text-white likeButton" style="font-size:30px;position: absolute; bottom: 6px; left: 8px">favorite_border</div>
+                        <div class="material-icons text-white likeButton" style="font-size:30px;position: absolute; bottom: 6px; left: 8px">favorite</div>
                     <?php endif;?>
                 </div>
 
