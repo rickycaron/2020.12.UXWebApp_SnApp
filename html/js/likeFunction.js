342ff05b07666c3c00936b1e2f363416b7aa2e8b
beiyang.li <beiyang.li@student.kuleuven.be>
2020-12-04 11:22:00 +0100

    likes function finished

--- html/js/likeFunction.js
+++ html/js/likeFunction.js
@@ -3,21 +3,21 @@ let likeStatus = document.getElementsByClassName("status");
 
 for (let i = 0; i < likeButton.length; i++) {
     likeButton[i].addEventListener('click', function() {
-        let likeS = likeStatus[i].parentElement.getAttribute("value")
 
-        if(likeS == 1) {
+
+        if(likeStatus[i].parentElement.getAttribute("value") == 1) {
             likeButton[i].className = "material-icons text-white likeButton"
             cancel_clicked(likeButton[i].parentElement.getAttribute("value"))
-            likeStatus[i] = 0
-            console.log(i);
-            console.log('likeStatus:', likeStatus[i]);
+            likeStatus[i].parentElement.setAttribute("value",0)
+
+            console.log('likeStatus:', likeStatus[i].parentElement.getAttribute("value"));
         }
-        else if(likeS == 0) {
+        else if(likeStatus[i].parentElement.getAttribute("value") == 0) {
             likeButton[i].className = "material-icons text-danger likeButton"
             accept_clicked(likeButton[i].parentElement.getAttribute("value"))
-            likeStatus[i] = 1
-            console.log(i);
-            console.log('likeStatus:', likeStatus[i]);
+            likeStatus[i].parentElement.setAttribute("value",1)
+
+            console.log('likeStatus:', likeStatus[i].parentElement.getAttribute("value"));
         }
     }, true);
 }
