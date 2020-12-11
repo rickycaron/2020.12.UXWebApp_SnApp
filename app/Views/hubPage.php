77b27efab4ad4c705f42885cecc9353472edc82c
maarten.kesters <maarten.kesters@student.kuleuven.be>
2020-12-11 10:35:40 +0100

    small fix

--- app/Views/hubPage.php
+++ app/Views/hubPage.php
@@ -27,13 +27,13 @@
             <input type = "hidden" name= "status" class = "status" value = "<?=$likeStatus?>">
         </div>
 
-        <div class="card my-2 shadow-sm " style="width:100%;max-width:600px">
+        <div class="card my-2 shadow-sm " style="width:100%; max-width:600px;">
 
 
-                <div style="position: relative;" value = "<?=$ob->id?>">
+                <div style="position: relative; object-fit: cover" value = "<?=$ob->id?>">
                     <a href="<?= base_url()?>/anobservation/<?=$ob->id?>">
-                    <img class="card-img" id="observationCardPicture" src="<?php echo data_uri($ob->imageData,$ob->imageType); ?>">
-                    <div class="card-img" style="box-shadow: inset 0px -50px 40px -20px black;position: absolute; width: 100%; height: 100%;top: 0; left: 0;"></div>
+                    <img class="card-img img-fluid" style="max-height: 500px" id="observationCardPicture" src="<?php echo data_uri($ob->imageData,$ob->imageType); ?>">
+                    <div class="card-img" style="box-shadow: inset 0px -50px 40px -20px black; position: absolute; width: 100%; height: 100%; top: 0; left: 0;"></div>
                     <h4 class="text-white" style="position: absolute; bottom: 0px; right: 12px;"><?=$ob->username?></h4>
                     <div class="material-icons text-white" style="font-size:30px;position: absolute; bottom: 6px; left: 8px" >favorite</div>
                     </a>
