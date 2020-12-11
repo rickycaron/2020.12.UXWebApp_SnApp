9cf856a15cd55f31b46d86402b0e88aad307cb8c
toon.luykx <toon.luykx@student.kuleuven.be>
2020-12-11 09:22:49 +0100

    spaced login text input

--- app/Views/login.php
+++ app/Views/login.php
@@ -4,18 +4,22 @@
         <?= session()->get('success') ?>
     </div>
 <?php endif; ?>
-<form class="form-signin" action="login" method="post">
+<form class="form" action="login" method="post">
     <?= csrf_field() ?>
-    <label for="email" class="sr-only">Email address</label>
-    <input type="email" id="inputEmail" class="form-control" placeholder="<?php echo lang('app.Email_address') ?>" name="email" value="<?= set_value("email")?>" required autofocus>
+    <div class="form-group mb-1">
+        <label for="email"><?php echo lang('app.Email_address') ?></label>
+        <input type="email" id="inputEmail" class="form-control" name="email" value="<?= set_value("email")?>" required autofocus>
+    </div>
 
-    <label for="password" class="sr-only">Password</label>
-    <input type="password" id="inputPassword" class="form-control" placeholder="<?php echo lang('app.Password') ?>" name="password" value="<?= set_value("password")?>" required>
+    <div class="form-group mb-1">
+    <label for="password"><?php echo lang('app.Password') ?></label>
+    <input type="password" id="inputPassword" class="form-control" name="password" value="<?= set_value("password")?>" required>
+    </div>
 
     <div class="d-flex justify-content-between">
 
-        <a class="text-info text-left w-100 mb-3" href="forgotPassword"><?php echo lang('app.Forgot_password?') ?></a>
-        <a class="text-info text-right w-100 mb-3" href="register"><?php echo lang('app.Register') ?></a>
+        <a class="text-info text-left w-100 mb-3 h6" href="forgotPassword"><?php echo lang('app.Forgot_password?') ?></a>
+        <a class="text-info text-right w-100 mb-3 h6" href="register"><?php echo lang('app.Register') ?></a>
     </div>
     <?php if ( isset($validation)): ?>
         <div class="alert alert-danger" role="alert">
@@ -27,7 +31,7 @@
             <?= $error_message?>
         </div>
     <?php endif; ?>
-    <button onclick=location.href='hub' class="btn btn-lg btn-primary btn-block mt-9" type="submit"><?php echo lang('app.Log_in') ?></button>
+    <button onclick=location.href='hub' class="btn btn-lg btn-primary btn-block mt-5" type="submit"><?php echo lang('app.Log_in') ?></button>
 </form>
 
 <hr class="mt-2 mb-3 my-3"/>
