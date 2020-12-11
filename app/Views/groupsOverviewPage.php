17d3b10fa13b4e6542277bac7ace71ce121e129e
toon.luykx <toon.luykx@student.kuleuven.be>
2020-12-11 09:43:39 +0100

    added light font

--- app/Views/groupsOverviewPage.php
+++ app/Views/groupsOverviewPage.php
@@ -2,7 +2,7 @@
 <div style="width:100%;max-width:600px">
 <?php foreach ($groups as $group): ?>
     <?php if ($group[3] == session()->get('id')):?>
-        <h4><?php echo lang('app.Groups_created_by_me') ?></h4>
+        <h4 class="font-light"><?php echo lang('app.Groups_created_by_me') ?></h4>
         <?php break;?>
     <?php endif?>
 <?php endforeach;?>
@@ -15,7 +15,7 @@
                 <div class="d-flex flex-row">
                     <div class="ml-3 mr-auto my-2 mt-3">
                         <h2><?=$group[0]?></h2>
-                        <p class="mb-1"><?=$group[1]?></p>
+                        <p class="mb-1 font-light"><?=$group[1]?></p>
                     </div>
                     <span class="material-icons my-auto mx-3" style="font-size: 40px">navigate_next</span>
                 </div>
@@ -32,7 +32,7 @@
 
     <?php foreach ($groups as $group): ?>
         <?php if ($group[3] !=session()->get('id')):?>
-            <h4><?php echo lang('app.Groups_I_joined') ?></h4>
+            <h4 class="font-light"><?php echo lang('app.Groups_I_joined') ?></h4>
             <?php break;?>
         <?php endif?>
     <?php endforeach;?>
--- app/Views/mainTemplate.php
+++ app/Views/mainTemplate.php
@@ -7,15 +7,12 @@
     <meta name="keywords" content="Animals Plants Nature Social media" />
     <meta name="description" content="This website is a social madia like page where you can share your observations from nature with your friends and the world" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--
-    <link href="<?= base_url()?>/css/main.css" rel="stylesheet"/>
-    <link href="<?= base_url()?>/css/reusable.css" rel="stylesheet"/>
-    <link href="<?= base_url()?>/css/pages.css?v=2" rel="stylesheet"/>
-    <link href="<?= base_url()?>/css/media_query.css" rel="stylesheet">
--->
+
     <link rel="stylesheet" href="<?= base_url()?>/css/bootstrap.css">
+    <link rel="stylesheet" href="<?= base_url()?>/css/main.css">
+
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@300;500&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <style>
@@ -64,7 +61,7 @@
     <?php endforeach; ?>
 </head>
 
-<body class="d-flex flex-column" style="min-height:100vh; font-family: 'Dosis', sans-serif; background-color: #006650">
+<body class="d-flex flex-column font-dark bg-primary">
 <header class="navbar fixed-top py-0" style=" max-height: 60px ;background-color: #006650; color: #FAFEFD">
     <span  class="material-icons my-auto" style="font-size:45px" id="header_icon_1"><?=$header_icon_1?></span>
     <h3><?=$title?></h3>
