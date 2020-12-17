7c4d3be9b5ad0da863d6ac9b7ed8e5ea9ca976de
maarten.kesters <maarten.kesters@student.kuleuven.be>
2020-12-17 13:19:40 +0100

    css groups

--- app/Views/groupsOverviewPage.php
+++ app/Views/groupsOverviewPage.php
@@ -2,7 +2,7 @@
 <div class="w-100">
 <?php foreach ($groups as $group): ?>
     <?php if ($group[3] == session()->get('id')):?>
-        <h4 class="font-light"><?php echo lang('app.Groups_created_by_me') ?></h4>
+        <h4 class="font-light pt-2"><?php echo lang('app.Groups_created_by_me') ?></h4>
         <?php break;?>
     <?php endif?>
 <?php endforeach;?>
@@ -41,12 +41,14 @@
 
 <?php foreach ($groups as $group): ?>
     <?php if ($group[3] !=session()->get('id')):?>
-        <div  class="card my-2 shadow-sm" style="width:100%;max-width:600px;">
-        <a class="w-100" style="color: black;" href="<?=$base_url?>/group/<?=$group[0]?>">
-            <div class="d-flex flex-row">
-                <div class="ml-3 mr-auto my-2 mt-3">
-                    <h2 class="font-light"><?=$group[0]?></h2>
-                    <p class="mb-1 font-light"><?=$group[1]?></p>
+        <div  class="card my-2 shadow-sm">
+            <a class="w-100" style="color: black;" href="<?=$base_url?>/group/<?=$group[0]?>">
+                <div class="d-flex flex-row">
+                    <div class="ml-3 mr-auto my-2 mt-3">
+                        <h2 class="font-light"><?=$group[0]?></h2>
+                        <p class="mb-1 font-light"><?=$group[1]?></p>
+                    </div>
+                    <span class="material-icons my-auto mx-3" style="font-size: 40px">navigate_next</span>
                 </div>
             </a>
             <div>
