7d83ddde5b1771b18532d92dbf49d125e01a15b4
joppe.leers <joppe.leers@student.kuleuven.be>
2020-12-17 20:52:29 +0100

    changed friend card

--- app/Views/friendList.php
+++ app/Views/friendList.php
@@ -17,6 +17,23 @@
 <?php if (count($requests)): ?>
 <h4><?php echo lang('app.Friend_List') ?></h4>
 <?php endif;?>
+
+<?php foreach ($friends as $f): ?>
+<a class="w-100 active" style="color: black;" href="<?=$base_url?>/otheruserprofile/<?= $f->id?>">
+    <div class="card my-2 shadow-sm d-flex flex-row" style="width:100%;max-width:600px">
+        <div class="d-flex flex-row">
+            <?php if(isset($f->p_imagedata)&&isset($f->p_imagetype)): ?>
+                <img class="personCardPhoto card-header ml-1 my-1 p-1 rounded-circle" width = "70" height="70" alt="Bootstrap Image Preview" src="<?=$f->encoded_image; ?>">
+            <?php else:?>
+                <img class="personCardPhoto card-header ml-1 my-1 p-1 rounded-circle" width = "70" height="70" alt="Bootstrap Image Preview" src="https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png">
+            <?php endif?>
+            <h4 class="my-auto m-3"><?= $f->username?></h4>
+        </div>
+        <a class="delete_friend material-icons my-auto mx-3" value="<?= $f->mappingID?>" style="font-size: 40px;">clear</a>
+    </div>
+</a>
+<?php endforeach?>
+<!--
 <?php foreach ($friends as $f): ?>
 <div class="card shadow my-2 w-100" style="max-width:400px">
     <?php if(isset($f->p_imagedata)&&isset($f->p_imagetype)): ?>
@@ -38,4 +55,4 @@
     </div>
 </div>
 <?php endforeach?>
-
+-->
