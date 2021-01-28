669d8763e7cc300fce83b8ec6f8d87502827f66c
maarten.kesters <maarten.kesters@student.kuleuven.be>
2021-01-28 17:34:49 +0100

    encoding

--- app/Views/hubPage.php
+++ app/Views/hubPage.php
@@ -42,7 +42,7 @@
                 <div style="position: relative; object-fit: cover" value = "<?=$ob->id?>">
                     <a href="<?= base_url()?>/anobservation/<?=$ob->id?>">
 
-                    <img class="card-img img-fluid " style="height: 350px; object-fit: cover;" src="<?='data:' . $ob->imageType . ';base64,' . base64_encode($ob->imageData)?>">
+                    <img class="card-img img-fluid " style="height: 350px; object-fit: cover;" src="<?=$ob->encoded_image?>">
                     <div class="card-img" style="box-shadow: inset 0px -50px 40px -20px black; position: absolute; width: 100%; height: 100%; top: 0; left: 0;"></div>
                     <h4 class="text-white" style="position: absolute; bottom: 0px; right: 12px;"><?=$ob->username?></h4>
                     <div class="material-icons text-white"  style="font-size:30px;position: absolute; bottom: 6px; left: 8px" >favorite</div>
